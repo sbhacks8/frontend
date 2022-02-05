963ca738a58ddb9eb15a9a099b68dd8edcdd8cf8
ezhang7423 <ezhang7423@student.palomar.edu>
2022-02-05 15:00:15 -0800

    earth

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import * as THREE from "three";
-import React, {
-  Suspense,
-  useEffect,
-  useState,
-  useRef,
-  forwardRef,
-} from "react";
+import React, { Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import {
-  Reflector,
-  Text,
-  useTexture,
-  OrbitControls,
-  useGLTF,
-  Stars,
-} from "@react-three/drei";
+import { OrbitControls, useGLTF, Stars } from "@react-three/drei";
 import "./App.css";
 
 function Earth(props) {
-  const { scene } = useGLTF("earth.glb");
+  const { scene } = useGLTF(".glb");
   return <primitive object={scene} {...props} />;
 }
 
@@ -28,7 +14,6 @@ export default function App() {
       <Canvas
         concurrent
         gl={{ alpha: false }}
-        pixelRatio={[1, 1.5]}
         camera={{ position: [0, 3, 100], fov: 15 }}
       >
         <OrbitControls />
@@ -39,7 +24,7 @@ export default function App() {
             position={[-1.2, 0, 0.6]}
             scale={[0.26, 0.26, 0.26]}
           />
-          
+
           <directionalLight position={[-20, 0, -10]} intensity={0.7} />
         </Suspense>
       </Canvas>
