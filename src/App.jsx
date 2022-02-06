76036cd2d4c3e4ba521756ee8ad6b6823614ed4e
Vincent Zhu <vincentzhu@ucsb.edu>
2022-02-05 21:50:26 -0800

    VZ Auth Part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
-import { useAuth0 } from '@auth0/auth0-react'
-import axios from "axios";
 
-function App() {
-  const {loginWithPopup, loginWithRedirect, logout, user, isAuthenticated, getAccessTokenSilently} = useAuth0()
-
-  function callApi() {
-    axios
-      .get("http://localhost:4000/")
-      .then(response => console.log(response.data))
-      .catch(error => console.log(error.message))
-  }
-  async function callProtectedApi() {
-    try {
-      const token = await getAccessTokenSilently();
-      const response = await axios.get("http://localhost:4000/protected", {
-        headers: {
-          'Authorization': `Bearer ${token}`
-        }
-      })
-      console.log(response.data);
-    } catch (error) {
-        console.log(error.message);
-    }
-    // axios
-    //   .get("http://localhost:4000/protected")
-    //   .then(response => console.log(response.data))
-    //   .catch(error => console.log(error.message))
-  }
-
+export default function App() {
   return (
     <div className="App">
-      <h1>Timely</h1>
-      <ul>
-        <li>
-          <button onClick={loginWithPopup}>Login With Popup</button>
-        </li>
-        <li>
-          <button onClick={loginWithRedirect}>Login With Redirect</button>
-        </li>
-        <li>
-          <button onClick={logout}>Logout</button>
-        </li>
-      </ul>
-      <p>You are { isAuthenticated ? "Logged in" : "Logged Out"}</p>
-
-      <ul>
-        <li><button onClick={callApi}>Call API</button></li>
-        <li><button onClick={callProtectedApi}>Call Protected API</button></li>
-      </ul>
-
-      { isAuthenticated && (<pre styled={{ textAlign: 'start' }}>{JSON.stringify(user, null, 2)}</pre> )}
-
       {/* <Router>
         <Routes>
           <Route exact path="/" element={<Landing />}></Route>
@@ -63,5 +14,3 @@ function App() {
     </div>
   )
 };
-
-export default App
