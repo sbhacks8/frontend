9a421eab163c9201bb0d50ff15f4615017dfee8d
ezhang7423 <ezhang7423@student.palomar.edu>
2022-02-05 18:04:08 -0800

    add rotation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,63 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, useGLTF, Stars } from "@react-three/drei";
 import "./App.css";
 
+const PERIOD = 100;
+
+function mult(array, v) {
+  return array.map((x) => x * v);
+}
+function rodrigues(theta, v, k) {
+  return math.add(
+    mult(v, Math.cos(theta)),
+    mult(math.cross(k, v), Math.sin(theta)),
+    mult(k, math.dot(k, v) * (1 - Math.cos(theta)))
+  );
+}
+
+function theta(time, period) {
+  return (2 * Math.PI * (time % period)) / period;
+}
+
 function Earth(props) {
-  const { scene } = useGLTF(".glb");
-  return <primitive object={scene} {...props} />;
+  const { scene } = useGLTF("earth.glb");
+  const [shiftEarth, setShiftEarth] = useState(0);
+
+  useFrame(({ clock }) => {
+    setShiftEarth(clock.getElapsedTime() / 5);
+  });
+  return (
+    <primitive
+      rotation={[0, Math.PI + shiftEarth, 0]}
+      position={[0, 0, 0]}
+      scale={[0.1, 0.1, 0.1]}
+      object={scene}
+      {...props}
+    />
+  );
+}
+
+function Light() {
+  const myLight = React.useRef();
+
+  const a = [-20, 30, 0];
+  const b = [0, 0, -20];
+  let planeVector = math.cross(a, b);
+  planeVector = planeVector.map((v) => {
+    return v / math.norm(planeVector);
+  });
+  useFrame(({ clock }) => {
+    let newPos = rodrigues(theta(clock.getElapsedTime(), 100), a, planeVector);
+
+    myLight.current.position.x = newPos[0];
+    myLight.current.position.y = newPos[1];
+    myLight.current.position.z = newPos[2];
+  });
+
+  return (
+    <directionalLight ref={myLight} position={[-20, 30, 0]} intensity={0.7} />
+  );
 }
 
 export default function App() {
@@ -14,18 +66,13 @@ export default function App() {
       <Canvas
         concurrent
         gl={{ alpha: false }}
-        camera={{ position: [0, 3, 100], fov: 15 }}
+        camera={{ position: [0, 0, 100], fov: 15 }}
       >
-        <OrbitControls />
+        {/* <OrbitControls /> */}
         <Stars />
         <Suspense fallback={null}>
-          <Earth
-            rotation={[0, Math.PI - 0.4, 0]}
-            position={[-1.2, 0, 0.6]}
-            scale={[0.26, 0.26, 0.26]}
-          />
-
-          <directionalLight position={[-20, 0, -10]} intensity={0.7} />
+          <Earth />
+          <Light />
         </Suspense>
       </Canvas>
     </>
